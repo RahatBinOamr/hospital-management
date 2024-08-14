a67a826304e00241fd89d311021138d69237ccbd
RahatBinOamr <108364543+RahatBinOamr@users.noreply.github.com>
2024-08-14 23:25:07 +0600

    create contact service and about tempalte and functions

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-from .views import home_page
+from .views import home_page,about_page,service_page,contact_page
 urlpatterns =[
   path('',home_page, name = 'home'),
+  path('about/',about_page, name = 'about'),
+  path('service/',service_page, name = 'service'),
+  path('contact/',contact_page, name = 'contact')
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
   return render(request, 'index.html')
+
+def about_page(request):
+  return render(request, 'about.html')
+
+
+def service_page(request):
+  return render(request, 'services.html')
+
+
+def contact_page(request):
+  return render(request, 'contact.html')
