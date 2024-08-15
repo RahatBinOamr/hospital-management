b61eaa990e5e9a7e40fed2556f7922e07c154a25
RahatBinOamr <108364543+RahatBinOamr@users.noreply.github.com>
2024-08-15 22:25:24 +0600

    create doctor department tempates and views

--- hospital/urls.py
+++ hospital/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path
-from .views import appointment_page
+from .views import appointment_page,doctor_page,single_doctor_page,department_page,single_department_page
+
+
+
 urlpatterns =[
-  path('appointment',appointment_page, name='appointment')
+  path('appointment',appointment_page, name='appointment'),
+  path('doctors',doctor_page, name='doctors'),
+  path('doctor', single_doctor_page, name='doctor'),
+  path('departments', department_page, name='departments'),
+  path('department', single_department_page,name='department'),
+
 ]
--- hospital/views.py
+++ hospital/views.py
@@ -3,3 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 def appointment_page(request):
   return render(request, 'appointment.html')
+
+def doctor_page(request):
+  return render(request, 'doctor.html')
+
+
+def single_doctor_page(request):
+  return render(request, 'single_doctor.html')
+
+
+def department_page(request):
+  return render(request, 'department.html')
+
+def single_department_page(request):
+  return render(request, 'single_department.html')
